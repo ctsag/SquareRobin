44b2b62d51053739f23f0249feb644bf15f847df
Christos Tsagkournis <chrtsago@yahoo.gr>
2013-08-11 20:23:06 +0300

    Created the CLITest unit test class

---
+++ src/gr/daemon/squarerobin/test/CLITest.java
@@ -0,0 +1,14 @@
+package gr.daemon.squarerobin.test;
+
+import gr.daemon.squarerobin.cli.CLI;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CLITest {
+	
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
