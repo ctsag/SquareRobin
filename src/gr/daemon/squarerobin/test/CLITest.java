c62ad3744595f245d8c31b2014a529e191cc9348
Christos Tsagkournis <chrtsago@yahoo.gr>
2013-08-12 18:48:49 +0300

    CLITest now also tests for odd and insufficient clubs as well as invalid arguments

--- src/gr/daemon/squarerobin/test/CLITest.java
+++ src/gr/daemon/squarerobin/test/CLITest.java
@@ -1,19 +1,54 @@
 package gr.daemon.squarerobin.test;
 
 import gr.daemon.squarerobin.cli.CLI;
+import gr.daemon.squarerobin.cli.State;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.Test;
+import java.security.Permission;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class CLITest {
+
+	private class ExitException extends SecurityException {
+
+		private int exitCode;
+
+		public ExitException(int code) {
+			super("System.exit(" + code + ") detected");
+			this.exitCode = code;
+		}
+		
+		public int getExitCode() {
+			return this.exitCode;
+		}
+
+	}
+
+	private class NoExitSecurityManager extends SecurityManager {
+		
+		public void checkPermission(Permission perm) {
+			// allow anything.
+		}
+
+		public void checkPermission(Permission perm, Object context) {
+			// allow anything.
+		}
+
+		public void checkExit(int status) {
+			super.checkExit(status);
+			throw new ExitException(status);
+		}
+
+	}
 	
 	private int countOccurences(String string, String substring) {
 		int count = 0;
 		int lastIndex = 0;				
-		
-		while (lastIndex != -1) {				
+
+		while (lastIndex != -1) {
 			lastIndex = string.indexOf(substring, lastIndex);
 			if (lastIndex != -1) {
 				count++;
@@ -24,12 +59,11 @@ public class CLITest {
 	}
 	
 	@Test
-	public void testMain() {
+	public void testProperExecution() {
 		String[] clubs = new String[]{"PAO", "OSFP", "AEK", "PAOK"};
 		String systemIn = "";
 		ByteArrayOutputStream systemOut = new ByteArrayOutputStream();
-		
-		
+
 		// Setup input and output.
 		for (String club : clubs) {
 			systemIn += club + "\n";
@@ -37,19 +71,73 @@ public class CLITest {
 		systemIn += "\n";
 		System.setIn(new ByteArrayInputStream(systemIn.getBytes()));		
 		System.setOut(new PrintStream(systemOut));
-		
-		// Call the main method
+
 		CLI.main(new String[]{});
-		
+
 		// Assert number of days
 		for (int i = 1; i < clubs.length; i++) {
 			assertEquals(1, this.countOccurences(systemOut.toString(), "Day " + i));			
 		}
-		
+
 		// Assert number of occurences per club
 		for (String club : clubs) {
 			assertEquals(clubs.length - 1, this.countOccurences(systemOut.toString(), "\r\n" + club + " - ") + this.countOccurences(systemOut.toString(), " - " + club + "\r\n"));
 		}
 	}
+	
+	
+	@Test
+	public void testOddClubs() {
+		String[] clubs = new String[]{"PAO", "OSFP", "AEK"};
+		String systemIn = "";		
+
+		// Setup input and output.
+		for (String club : clubs) {
+			systemIn += club + "\n";
+		}
+		systemIn += "\n";
+		System.setIn(new ByteArrayInputStream(systemIn.getBytes()));
+
+		// Assert odd number of clubs produces appropriate exit code
+		System.setSecurityManager(new NoExitSecurityManager());
+		try {
+			CLI.main(new String[]{});
+			fail("System.exit() expected");
+		} catch(ExitException e) {			
+			assertEquals(State.ODD_CLUBS.getValue(), e.getExitCode());
+		}
+		System.setSecurityManager(null);
+	}
+
+	@Test
+	public void testInsufficientClubs() {
+		String systemIn = "\n\n";
+
+		// Setup input and output.
+		System.setIn(new ByteArrayInputStream(systemIn.getBytes()));
+
+		// Assert insufficient number of clubs produces appropriate exit code
+		System.setSecurityManager(new NoExitSecurityManager());
+		try {
+			CLI.main(new String[]{});
+			fail("System.exit() expected");
+		} catch(ExitException e) {
+			assertEquals(State.INSUFFICIENT_CLUBS.getValue(), e.getExitCode());
+		}
+		System.setSecurityManager(null);		
+	}
+	
+	@Test
+	public void testInvalidArguments() {
+		// Assert invalid arguments number of clubs produce appropriate exit code
+		System.setSecurityManager(new NoExitSecurityManager());
+		try {
+			CLI.main(new String[]{"invalid"});
+			fail("System.exit() expected");
+		} catch(ExitException e) {
+			assertEquals(State.INVALID_ARGUMENTS.getValue(), e.getExitCode());
+		}		
+		System.setSecurityManager(null);
+	}	
 
 }
