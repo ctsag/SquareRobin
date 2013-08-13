460c32f4110064e7af1a987c477beeb7c2583cfd
tnik <tnik@illusion>
2013-08-13 17:43:17 +0300

    ScheduleTest: added @Before

--- src/test/gr/daemon/squarerobin/test/SchedulerTest.java
+++ src/test/gr/daemon/squarerobin/test/SchedulerTest.java
@@ -1,30 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gr.daemon.squarerobin.test;
 
 import gr.daemon.squarerobin.model.Scheduler;
 import java.util.ArrayList;
 import java.util.HashMap;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author tnik
- */
 public class SchedulerTest {
+
+    private ArrayList<String> teams = new ArrayList<>();
+    private HashMap<Integer, ArrayList<String[]>> outSchedule = new HashMap<>();
+    
+    @Before
+    public void setUp() {
+        teams = new ArrayList<>();
+        outSchedule = new HashMap<>();
+    }
     
     @Test
     public void testOddTeams() {
-        
-        ArrayList<String> teams = new ArrayList<>();
-        HashMap<Integer, ArrayList<String[]>> outSchedule = new HashMap<>();
         
         teams.add("PAO");
         teams.add("OSFP");
@@ -43,9 +38,6 @@ public class SchedulerTest {
     @Test
     public void testEmptyInput() {
         
-        ArrayList<String> teams = new ArrayList<>();
-        HashMap<Integer, ArrayList<String[]>> outSchedule = new HashMap<>();
-        
         try {
             Scheduler scheduler = new Scheduler(teams);
             outSchedule = scheduler.getSchedule();
@@ -58,9 +50,6 @@ public class SchedulerTest {
     
     @Test
     public void testNotUniqueTeams() {
-        
-        ArrayList<String> teams = new ArrayList<>();
-        HashMap<Integer, ArrayList<String[]>> outSchedule = new HashMap<>();
         
         teams.add("PAO");
         teams.add("OSFP");
@@ -79,9 +68,6 @@ public class SchedulerTest {
     
     @Test
     public void testOutput() {
-        
-        ArrayList<String> teams = new ArrayList<>();
-        HashMap<Integer, ArrayList<String[]>> outSchedule = new HashMap<>();
         
         teams.add("PAO");
         teams.add("OSFP");
