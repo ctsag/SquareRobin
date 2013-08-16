a6bf4d6e68ef0c0c07c1ec59715f6350da8d0799
Christos Tsagkournis <chrtsago@yahoo.gr>
2013-08-17 01:07:45 +0300

    Replaced the input text area with a table

--- src/java/gr/daemon/squarerobin/gui/SquareRobin.java
+++ src/java/gr/daemon/squarerobin/gui/SquareRobin.java
@@ -1,15 +1,13 @@
 package gr.daemon.squarerobin.gui;
 
-import gr.daemon.squarerobin.cli.State;
 import gr.daemon.squarerobin.model.Scheduler;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,6 +17,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class SquareRobin extends JFrame implements ActionListener {
 
@@ -26,9 +26,10 @@ public class SquareRobin extends JFrame implements ActionListener {
 	private static final String APPLICATION_NAME = "Square Robin";
 	private JPanel contentPane;
 	private JButton runButton;
-	private JTextArea inputTextArea;
 	private JTextArea matchesTextArea;
 	private ArrayList<String> clubs = new ArrayList<>();
+	private JTable inputTable;
+	private JScrollPane inputScrollPane;
 
 	public SquareRobin() {
 		try {
@@ -41,10 +42,14 @@ public class SquareRobin extends JFrame implements ActionListener {
 	}
 	
 	private void parseInput() {
+		String cell = "";
+		
+		this.clubs.clear();
 		this.matchesTextArea.setText("");
-		for (String line : this.inputTextArea.getText().split("\n")) {
-			if (!line.equals("\n")) {
-				this.clubs.add(line);
+		for (int i = 0; i < this.inputTable.getRowCount(); i++) {
+			cell = (String) this.inputTable.getValueAt(i, 0);
+			if (cell != null && !cell.equals("")) {
+				this.clubs.add(cell);
 			}
 		}
 	}
@@ -70,20 +75,27 @@ public class SquareRobin extends JFrame implements ActionListener {
 	}
 	
 	private void initComponents() {
+		String[] columns = { "Club" };
+		DefaultTableModel inputModel = new DefaultTableModel(256, columns.length);
+		inputModel.setColumnIdentifiers(columns);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(100, 100, 550, 700);
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.setContentPane(contentPane);
+		this.setContentPane(contentPane);		
 		this.runButton = new JButton("Go!");
 		this.runButton.addActionListener(this);
-		this.inputTextArea = new JTextArea();		
-		this.matchesTextArea = new JTextArea();
+		this.matchesTextArea = new JTextArea();		
+		this.inputTable = new JTable();		
+		this.inputTable.setModel(inputModel);
+		this.inputTable.putClientProperty("terminateEditOnFocusLost", true);
+		this.inputScrollPane = new JScrollPane();
+		this.inputScrollPane.setViewportView(this.inputTable);
 		this.setMinimumSize(new Dimension(900, 700));
 		this.setTitle(SquareRobin.APPLICATION_NAME);
 	}
 	
-	private void initLayoutManager() {		
+	private void initLayoutManager() {
 		GroupLayout gl_contentPane = new GroupLayout(this.contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -93,21 +105,20 @@ public class SquareRobin extends JFrame implements ActionListener {
 							.addContainerGap()
 							.addComponent(this.runButton))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(4)
-							.addComponent(this.inputTextArea, GroupLayout.PREFERRED_SIZE, 292, GroupLayout.PREFERRED_SIZE)
+							.addComponent(this.inputScrollPane, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(this.matchesTextArea, GroupLayout.PREFERRED_SIZE, 292, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(280, Short.MAX_VALUE))
+					.addContainerGap(285, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(this.inputTextArea, GroupLayout.PREFERRED_SIZE, 512, GroupLayout.PREFERRED_SIZE)
-						.addComponent(this.matchesTextArea, GroupLayout.PREFERRED_SIZE, 512, GroupLayout.PREFERRED_SIZE))
+						.addComponent(this.inputScrollPane, GroupLayout.PREFERRED_SIZE, 514, GroupLayout.PREFERRED_SIZE)
+						.addComponent(this.matchesTextArea, GroupLayout.DEFAULT_SIZE, 513, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(this.runButton)
-					.addContainerGap(111, Short.MAX_VALUE))
+					.addContainerGap(110, Short.MAX_VALUE))
 		);
 		this.contentPane.setLayout(gl_contentPane);
 	}
@@ -130,5 +141,4 @@ public class SquareRobin extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new SwingStartUp());
 	}
-
 }
