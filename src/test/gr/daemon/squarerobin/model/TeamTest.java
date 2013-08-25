bcc7f7041563644cd18dba70a52a1847de22e2be
Christos Tsagkournis <chrtsago@yahoo.gr>
2013-08-25 20:32:10 +0300

    Implemented unit tests for Team win/lose/draw API

--- src/test/gr/daemon/squarerobin/model/TeamTest.java
+++ src/test/gr/daemon/squarerobin/model/TeamTest.java
@@ -63,9 +63,94 @@ public class TeamTest {
 	public void testGoalAverage() {		
 		final int expected = TeamTest.GOALS_FOR - TeamTest.GOALS_AGAINST;
 		final Team team = new Team(TeamTest.TEAM_A);
+
 		team.setGoalsFor(TeamTest.GOALS_FOR);
 		team.setGoalsAgainst(TeamTest.GOALS_AGAINST);
 		assertEquals(expected, team.getGoalAverage());
+	}
+
+	@Test
+	public void testGoalsForOnWin() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.win(1, 0);
+		assertEquals(4, team.getGoalsFor());		
+	}
+
+	@Test
+	public void testGoalsAgainstOnWin() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsAgainst(2);
+		team.win(2, 1);
+		assertEquals(3, team.getGoalsAgainst());		
+	}
+
+	@Test
+	public void testGoalAverageOnWin() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.setGoalsAgainst(2);
+		team.win(3, 1);
+		assertEquals(3, team.getGoalAverage());		
+	}
+
+	@Test
+	public void testGoalsForOnLose() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.lose(1, 2);
+		assertEquals(4, team.getGoalsFor());		
+	}
+
+	@Test
+	public void testGoalsAgainstOnLose() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsAgainst(2);
+		team.lose(1, 2);
+		assertEquals(4, team.getGoalsAgainst());		
+	}
+
+	@Test
+	public void testGoalAverageOnLose() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.setGoalsAgainst(2);
+		team.lose(1, 3);
+		assertEquals(-1, team.getGoalAverage());		
+	}
+	
+	@Test
+	public void testGoalsForOnDraw() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.draw(1, 1);
+		assertEquals(4, team.getGoalsFor());		
+	}
+
+	@Test
+	public void testGoalsAgainstOnDraw() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsAgainst(2);
+		team.draw(2, 2);
+		assertEquals(4, team.getGoalsAgainst());		
+	}
+
+	@Test
+	public void testGoalAverageOnDraw() {
+		final Team team = new Team(TeamTest.TEAM_A);
+
+		team.setGoalsFor(3);
+		team.setGoalsAgainst(2);
+		team.draw(1, 1);
+		assertEquals(1, team.getGoalAverage());		
 	}
 
 }
